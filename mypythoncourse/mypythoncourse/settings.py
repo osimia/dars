b8a2d94d3e5ebad987ca6383f9ed39c21af2f948
Akramkhon Osimi <6721067659@my.lerna.tj>
2025-07-11 21:36:03 +0500

    Fix deployment configuration for Railway

--- mypythoncourse/mypythoncourse/settings.py
+++ mypythoncourse/mypythoncourse/settings.py
@@ -26,9 +26,9 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # SECRET_KEY и DEBUG
 SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = os.getenv("DEBUG") == "True"
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]  # Для Railway
 
 
 # Application definition
@@ -129,7 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
